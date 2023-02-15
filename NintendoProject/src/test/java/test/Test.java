3c95ad30a20836e3f274d3f7de4feac6e45dfcc4
teddybengoto <teddybengoto@gmail.com>
2023-02-15 17:12:35 +0100

    Add Jeu

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,9 +1,23 @@
 package test;
 
+import model.Console;
+import model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Console c = new Console("Console");
+
+
+		Jeu j1 = new Jeu("Jeu1", c);
+		Jeu j2 = new Jeu("Jeu1", c);
+		Jeu j3 = new Jeu("Jeu1", c);
+		Jeu j4 = new Jeu("Jeu1", c);
+		Jeu j5 = new Jeu("Jeu1", c);
+
+
 
 	}
 
