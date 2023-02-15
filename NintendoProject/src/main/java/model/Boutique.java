ec8a684ccedf21464b32e7715ec79d0407940fb0
ThomasAbrial <thomas.abrial@laposte.net>
2023-02-15 17:04:32 +0100

    j'avais oublie les constructeur gette setter

--- NintendoProject/src/main/java/model/Boutique.java
+++ NintendoProject/src/main/java/model/Boutique.java
@@ -3,4 +3,29 @@ package model;
 public class Boutique {
 	private String nom;
 	private Adresse adresse;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Adresse getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + "]";
+	}
+	public Boutique(String nom, Adresse adresse) {
+		super();
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+	
+	
+	
 }
